8ddea745b7b7786a29b35fb1c4bf518fc018b8ac
Vera van Vloten <veravanvloten@gmail.com>
2022-10-05 03:53:25 +0200

    Added an Gif

--- scripts/script.js
+++ scripts/script.js
@@ -27,3 +27,22 @@ function handleKeydown(event) {
     deNav.classList.remove("toonMenu");
   }
 }
+
+
+// animatie
+
+const vliegtuigButton = document.querySelector("section:nth-of-type(1) p");
+const vliegtuigImg = document.querySelector("main > img");
+const audio = document.querySelector("main > audio");
+
+vliegtuigButton.addEventListener("click", function () {
+    if (vliegtuigImg.classList.contains("hidden")){
+        vliegtuigImg.classList.remove("hidden");
+        audio.play();
+    } else {
+        vliegtuigImg.classList.add("hidden");
+        audio.pause();
+        audio.currentTime = 0;
+    }
+})
+
